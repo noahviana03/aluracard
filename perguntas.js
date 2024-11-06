be4840481906507d14948d168b2a70f9654d2c7f
noahviana03 <00001118473358sp@al.educacao.sp.gov.br>
2024-11-06 08:26:29 -0300

    Noah

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'qual e o principal objetivo da inteligencia artificial ?',
+    'criar maquinas que possam operar com o mesmo nivel de capacidade que os humanos'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
